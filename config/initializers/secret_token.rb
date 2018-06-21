6315ac1175736914a8cd082e260c33ab34074a22
Nicole Bolton <nmb321@lehigh.edu>
2018-06-21 10:15:43 -0400

    Initiate Blog App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = 'a90641bb0a8b91ff42d2acc2798836bc72083cc3d5a7a3a25435b16f1afe9134d3ac445a5f4ac52948331e108d44a5a7936a08c776cfadacb31b043f94fcb2a4'
